4c5e701f59b563e0a78ac07b2d100d77a81e0d6f
Gullu-zada <gahmadzada16274@ada.edu.az>
2026-02-20 09:22:54 +0400

    Add method to reverse a string

---
+++ String.java
@@ -0,0 +1,9 @@
+public static String reverseString(String str) {
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        sb.append(str.charAt(i));
+    }
+
+    return sb.toString();
+}
